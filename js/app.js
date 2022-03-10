bbb89ffa0dc0cd949d058393b3c911ba3c2cf591
Rasel <azad.is.rasel@gmail.com>
2022-03-11 02:09:37 +0600

    read more problem solved

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-  return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+  return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
 const switchTab = (id) => {
@@ -52,6 +52,7 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
+  console.log(post)
   const image = post.image;
   const div = document.createElement("article");
   div.classList.add("post");
